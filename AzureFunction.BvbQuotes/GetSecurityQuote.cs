4102864a71e9b091a531155bceb3df41c807ba88
TheNila74 <danilandrei@proton.me>
2025-05-20 20:12:01 +0300

    Changed project type to Azure function

---
+++ AzureFunction.BvbQuotes/GetSecurityQuote.cs
@@ -0,0 +1,30 @@
+using BvbQuotes.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunction.BvbQuotes
+{
+    public class GetSecurityQuote
+    {
+        private readonly ILogger<GetSecurityQuote> _logger;
+
+        public GetSecurityQuote(ILogger<GetSecurityQuote> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("quote")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        {
+            var symbol = req.Query["symbol"].ToString() ?? string.Empty;
+
+            var wpd = new WebPageDownloader();
+            var quote = wpd.GetQuoteForSecurity(symbol);
+
+            if (string.IsNullOrEmpty(symbol)) return new NotFoundObjectResult("Security not found");
+            else return new OkObjectResult(quote);
+        }
+    }
+}
---
+++ AzureFunction.BvbQuotes/WebPageDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using HtmlAgilityPack;
+
+namespace BvbQuotes.Services;
+
+public class WebPageDownloader
+{
+    public string GetQuoteForSecurity(string security)
+    {
+        var htmlDoc = DownloadHtmlForSecurity(security);
+        return SelectQuoteForSecurity(htmlDoc);
+    }
+    
+    public string DownloadHtmlForSecurity(string security)
+    {
+        var url = GetFullUrl(security);
+        
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", 
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+
+
+        var result = string.Empty;
+        try
+        {
+            var response = client.GetAsync(url).Result;
+            result = response.Content.ReadAsStringAsync().Result;
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error fetching page: {ex.Message}");
+        }
+        return result;
+    }
+    
+    public string GetFullUrl(string security)
+    {
+        var uri = new Uri(BASE_URL);
+        var uriBuilder = new UriBuilder(uri);
+        uriBuilder.Query = $"s={security}";
+        
+        return uriBuilder.Uri.ToString();
+    }
+
+    private string SelectQuoteForSecurity(string htmlDocument)
+    {
+        // Load HTML into parser
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(htmlDocument);
+
+        var valueNode = htmlDoc.DocumentNode.SelectSingleNode("//b[@class='value']");
+
+        return valueNode.InnerText;
+    }
+
+    public const string BASE_URL = "https://bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx";
+}
