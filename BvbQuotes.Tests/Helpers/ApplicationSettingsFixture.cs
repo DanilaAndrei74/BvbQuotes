4b68ce31bd3cc7027bd99a824a35da82a707bdb2
TheNila74 <danilandrei@proton.me>
2025-06-09 11:59:51 +0300

    Added simple test in test project

---
+++ BvbQuotes.Tests/Helpers/ApplicationSettingsFixture.cs
@@ -0,0 +1,30 @@
+﻿using BvbQuotes.Functions.Configuration;
+
+namespace BvbQuotes.Tests.Helpers;
+
+public class ApplicationSettingsFixture
+{
+    public ApplicationSettingsFixture()
+    {
+        // Set the static properties before tests run
+        ApplicationSettings.WebConfiguration = new WebConfiguration
+        {
+            BvbUrl = "https://bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx",
+            UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            Culture = "ro-RO"
+        };
+
+        ApplicationSettings.BusinessLogicConfiguration = new BusinessLogicConfiguration
+        {
+            QuoteCss = "//b[@class='value']",
+            DateCss = "//span[@class='date small']"
+        };
+    }
+}
+
+[CollectionDefinition("AppSettings collection")]
+public class AppSettingsCollection : ICollectionFixture<ApplicationSettingsFixture>
+{
+    // This class has no code, and is never created.
+    // Its purpose is just to be the [CollectionDefinition]
+}
---
+++ BvbQuotes.Tests/Services/WebPageDownloaderTests.cs
@@ -0,0 +1,30 @@
+﻿using BvbQuotes.Functions.Configuration;
+using BvbQuotes.Functions.Services;
+using BvbQuotes.Tests.Helpers;
+
+namespace BvbQuotes.Tests.Services;
+
+[Collection("AppSettings collection")]
+public class WebPageDownloaderTests
+{
+    private readonly WebPageDownloader _sut;
+    
+    public WebPageDownloaderTests()
+    {
+        _sut = new WebPageDownloader();
+    }
+
+    [Fact]
+    public void Wpd_Should_Return_Result()
+    {
+        var result = _sut.GetQuoteForSecurity("TLV");
+        Assert.NotNull(result);
+    }
+    
+    // [Fact]
+    // public void Wpd_GetFullUrl()
+    // {
+    //     var result = _sut.GetFullUrl();
+    //     Assert.NotNull(result);
+    // }
+}
